205d4d434d80ddf8d1c795f42000ee8e8cbec493
jinqing808 <jinqing808@190398b0-3e5a-db71-e392-7b5dfaa953ca>
2010-11-05 04:07:57 +0000

    finished

--- trunk/CajoTest/src/SomeClient.java
+++ trunk/CajoTest/src/SomeClient.java
@@ -1,19 +1,103 @@
 import gnu.cajo.invoke.Remote;
 
-import java.sql.Timestamp;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
 
 public class SomeClient {
+	
    public static void main(String args[]) throws Exception {
-      Object object = Remote.getItem("//localhost:1198/someName2");
-      byte[] arr = new byte [100000];
-      for(int i=0;i<arr.length;i++)
-    	  arr[i] = (byte)i;
+      HashMap<String, Object> machineObjectMap = new HashMap<String, Object> ();
+    
+    try{
+      FileReader rdFile = new FileReader("machines");
+      BufferedReader brdFile = new BufferedReader(rdFile);
+
+
+      String strLine;
+      String connectpreFix = "//";
+      String connectpostFix = ":1198/someName";
+
+      while ((strLine = brdFile.readLine()) != null) {
+    	  strLine = strLine.trim();
+    	  if(strLine.equals(""))
+    		  continue;
+
+    	  try{
+    		  Object o = Remote.getItem(connectpreFix + strLine + connectpostFix); 
+    		  machineObjectMap.put(strLine, o);
+    	  }
+    	  catch(Exception e){
+    		  System.out.println("machine " + strLine + " not connected!");
+    	  }
+      }
+
+      brdFile.close();
+
+    }
+
+    catch (IOException e) {
+
+      e.printStackTrace();
+
+    }
+    
       
-      System.out.println(new Timestamp(System.currentTimeMillis()));
+
+      String s = "";
       
-      Remote.invoke(object, "test", arr);
+      InputStreamReader input = new InputStreamReader(System.in);
+      BufferedReader reader = new BufferedReader(input);
       
-      System.out.println(new Timestamp(System.currentTimeMillis()));
+      
+      while(true){
+	      try{
+	    	  s = reader.readLine();
+	      }
+	      catch(Exception e){}
+	      
+	      s = s.trim();
+	      
+	      if(s.equals(""))
+	    	  continue;
+	      
+	      if(s.equals("exit"))
+	    	  break;
+	      
+	      String[] p = s.split(" ");
+	      if(p.length != 2){
+	    	  System.out.println("command error!");
+	    	  continue;
+	      }
+	      
+	      try{
+		      Object tempObj = null;
+		      if(!p[1].equals("all")){
+		    	  tempObj = machineObjectMap.get(p[1]);
+		    	  Remote.invoke(tempObj, p[0], null);
+		      }
+		      else{
+		    	  Iterator it = machineObjectMap.entrySet().iterator();
+		    	  while(it.hasNext()){
+		    		  Entry entry = (Entry) it.next();
+		    		  tempObj = entry.getValue();
+		    		  Remote.invoke(tempObj, p[0], null);
+		    	  }
+		      }
+	      }catch(Exception e){
+	    	  e.printStackTrace();
+	    	  continue;
+	      }
+	    	  
+      }
+      
+      
+      
+      //Remote.shutdown();
    }
 }
 
